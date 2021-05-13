46f823ed49bb820d9da248391754118ebc3ce5a2
dinhhao2584 <nguyendinhhao.iuh@gmail.com>
2021-05-13 22:37:28 +0700

    Finish Test

---
+++ NguyenDinhHao_17029171/UTLab12.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Text;
+
+namespace NguyenDinhHao_17029171
+{
+    [TestClass]
+    public class UTLab12
+    {
+        private TestContext testContext;
+
+        public TestContext TestContext { get => testContext; set => testContext = value; }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+        @"|DataDirectory|\Data\UTLab12.csv", "UTLab12#csv", DataAccessMethod.Sequential),
+        DeploymentItem(@"Data\UTLab12.csv"), TestMethod]
+        public void TestLargest()
+        {
+            MethodLibrary.MethodLibrary method = new MethodLibrary.MethodLibrary();
+            int[] aNguyen = new int[3];
+
+           
+            //
+            if (!String.IsNullOrEmpty(TestContext.DataRow[1].ToString()))
+            {
+                string arrays = TestContext.DataRow[1].ToString();
+                string[] separatingStrings = { "12"};
+                string[] arrs = arrays.Split(separatingStrings, System.StringSplitOptions.RemoveEmptyEntries);
+                
+                for (int i = 0; i < arrs.Length; i++)
+                {
+                    aNguyen[i] = Convert.ToInt32(arrs[i]);
+                }
+            }
+            else
+            {
+                aNguyen = new int[] { };
+            }
+            
+            int ActualMax = method.Largest(aNguyen);
+
+            int ExpecMax = Convert.ToInt32(TestContext.DataRow[2].ToString());
+            
+
+            Assert.AreEqual(ExpecMax, ActualMax);
+        }
+    }
+}
