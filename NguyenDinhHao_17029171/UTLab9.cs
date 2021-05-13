527cd649fe0ba2ec9367dee113a836ed1d5afc08
dinhhao2584 <nguyendinhhao.iuh@gmail.com>
2021-05-13 18:04:29 +0700

    UTLab9

---
+++ NguyenDinhHao_17029171/UTLab9.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace NguyenDinhHao_17029171
+{
+    [TestClass]
+    public class UTLab9
+    {
+        private TestContext testContext;
+
+        public TestContext TestContext { get => testContext; set => testContext = value; }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+        @"|DataDirectory|\Data\UTLab9.csv", "UTLab9#csv", DataAccessMethod.Sequential),
+        DeploymentItem(@"Data\UTLab9.csv"), TestMethod]
+        public void TestSum()
+        {
+            MethodLibrary.MethodLibrary method = new MethodLibrary.MethodLibrary();
+            long S0 = long.Parse(TestContext.DataRow[1].ToString());
+            long ActualS;
+            long ActualK = method.Sum(S0, out ActualS);
+
+            long ExpecS = long.Parse(TestContext.DataRow[3].ToString());
+            long ExpecK = long.Parse(TestContext.DataRow[2].ToString());
+
+            Assert.AreEqual(ExpecK, ActualK);
+            Assert.AreEqual(ExpecS, ActualS);
+
+        }
+    }
+}
